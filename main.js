3d4f806ec5741fbe5143839312dc46078a6601df
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-04-12 22:16:12 -0500

    Rework layout and resize margines for tasks.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const add_button = document.getElementById("btn_add");
 const task_input = document.getElementById("user_input");
+
+task_input.setAttribute('size',task_input.getAttribute('placeholder').length);
+
 add_button.onclick = function() {
     const para = document.createElement("p");
-    para.innerText = task_input.value;
+    para.innerText = "- " + task_input.value;
     document.body.appendChild(para);
     task_input.value = "";
     task_input.focus();
